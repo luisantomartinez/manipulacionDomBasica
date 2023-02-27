897e1064d69ceddf895212fd58f1f0fcb4afd23b
antonio <antonio.martinez130520@gmail.com>
2023-02-27 15:26:15 -0600

    agregar .gitignore y archivos

---
+++ script.js
@@ -0,0 +1,15 @@
+const h1=document.querySelector('h1');
+const form=document.querySelector('#form');
+const input1=document.querySelector('#calculo1');
+const input2=document.querySelector('#calculo2');
+const btn=document.querySelector('#btnCalcular');
+const pResult=document.querySelector('#result');
+
+
+btn.addEventListener('submit', sumaInputValues)
+
+function sumaInputValues(event){
+    event.preventDefault();
+    const sumaInputs = input1.value + input2.value;
+    pResult.innerText="Resultado: "+ sumaInputs;
+}
